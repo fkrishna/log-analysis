d11be4e46ace8960367d88cab55bcd708224a11a
Krishna Farvil <fkrishna.dev@gmail.com>
2019-03-04 13:08:44 -0800

    Feat: retrieve most popular article authors

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ import psycopg2
 #######################
 # DB SERVICES 
 #######################
+
 DBNAME = "news"
+
 def db_connection():
     db = psycopg2.connect(database=DBNAME)
     return db.cursor()
 
-def db_get_top_articles(limit):
+# most articles that have been accessed the most
+def db_get_popular_articles(limit):
     cursor = db_connection()
     query = """
         SELECT title, 
@@ -17,6 +20,23 @@ def db_get_top_articles(limit):
         GROUP BY title, path 
         ORDER BY nviews DESC LIMIT {}
     """.format(limit)
+
+    cursor.execute(query)
+    return cursor.fetchall()
+
+
+# most popular article authors of all time
+def db_get_popular_authors():
+    cursor = db_connection()
+    query = """
+        SELECT name, 
+        COUNT(*) as nviews
+        FROM authors, articles, log
+        WHERE authors.id = articles.author
+        AND log.path = CONCAT('/article/', articles.slug)
+        GROUP BY name
+        ORDER BY nviews DESC
+    """
     cursor.execute(query)
     return cursor.fetchall()
 
@@ -34,10 +54,18 @@ def format_num(num):
 #######################
 # OUTPUT 
 #######################
-def output_top_articles():
+def output_popular_articles():
     print("\n- Top 3 Most Popular Articles\n")
-    for items in db_get_top_articles(3):
+    for items in db_get_popular_articles(3):
         print("Title: " + str(items[0]))
+        print("Views: " + format_num(items[1]))
+        separator()
+
+
+def output_popular_auhtors():
+    print("\n- Most Popular Article Authors\n")
+    for items in db_get_popular_authors():
+        print("Author Name: " + str(items[0]))
         print("Views: " + format_num(items[1]))
         separator()
 
@@ -46,7 +74,8 @@ def output_top_articles():
 
 def main():
     print("\n** LOGS ANALYSIS **")
-    output_top_articles()
+    output_popular_articles()
+    output_popular_auhtors()
 
 if __name__ == '__main__':
     main()
